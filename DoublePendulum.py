c2d77054874326d192b5cc470c5ea27dd51945b8
Sumaiya Tahsin <stahsin@smith.edu>
2025-07-21 14:41:03 -0400

    Update DoublePendulum.py

--- DoublePendulum.py
+++ DoublePendulum.py
@@ -6,6 +6,7 @@ from joblib import Parallel, delayed
 import os
 import multiprocessing
 import time
+from scipy.interpolate import RectBivariateSpline
 total_start = time.time()
 
 print("Here I start again!")
@@ -14,15 +15,18 @@ print("Here I start again!")
 print("🧠 CPUs visible to Python:", multiprocessing.cpu_count())
 print("🔧 CPUs allocated by SLURM:", os.environ.get("SLURM_CPUS_ON_NODE"))
 
-m1=10000
-m2=10
+m1=1000
+m2=1
 g=10
-l1=0.01
-l2=1
-w=np.sqrt(g/l2)
-u1=0
-u2=0.01
+l1=1
+l2=10
+I1=m1*l1**2
+I2=m2*l2**2
+w1=np.sqrt(g/l1)
+w2=np.sqrt(g/l2)
 h_bar=1
+u1=0
+u2=0
 f1=0.0001
 f2=0.1
 
@@ -32,23 +36,20 @@ b = m2*l1*l2
 c = m2*l2
 
 # Spatial and momentum grid parameters
-o1_min, o1_max = -3,3
-o2_min, o2_max = -3,3
-n_o1, n_o2 = 50, 50
+o1_min, o1_max = -0.08,0.08
+o2_min, o2_max = -0.8,0.8
+n_o1, n_o2 = 161, 161
 
-p1_min, p1_max = -3,3
-p2_min, p2_max = -3,3
-n_p1, n_p2 = 50, 50
+n1 = np.arange(-60, 61)
+n2 = np.arange(-6, 7)
 
-y_min, y_max = -5, 5
-n_y = 100  # number of points for integration grid
-y1_vals = np.linspace(y_min, y_max, n_y)
-y2_vals = np.linspace(y_min, y_max, n_y)
+n_y = 200  # number of points in y grid for integration
+y1_vals = np.linspace(-np.pi/2, np.pi/2, n_y)
+y2_vals = np.linspace(-np.pi/2, np.pi/2, n_y)
 
 dO1 = (o1_max - o1_min) / (n_o1 - 1)
 dO2 = (o2_max - o2_min) / (n_o2 - 1)
-dp1 = (p1_max - p1_min) / (n_p1 - 1)
-dp2 = (p2_max - p2_min) / (n_p2 - 1)
+
 
 t_f=0  #final time
 dt=0.0001
@@ -57,26 +58,22 @@ n_t=math.ceil(t_f/dt) #Round Up
 # Create coordinate grids
 o1 = np.linspace(o1_min, o1_max, n_o1)
 o2 = np.linspace(o2_min, o2_max, n_o2)
-p1 = np.linspace(p1_min, p1_max, n_p1)
-p2 = np.linspace(p2_min, p2_max, n_p2)
 
 D = (-h_bar**2/ 2)*(1/((a*c)-(c**2)-(b**2)*(np.cos(o2)**2)))
 A = c*D
 B = a*D + 2*b*D*np.cos(o2)
 C = 2*c*D + 2*b*D*np.cos(o2)
 
-U = -m1*g*l1*np.cos(o1) - m2*g*(l1*np.cos(o1) + l2*np.cos(o2))
 
 def Psi(O1,O2,f1,f2,u1,u2):
     G = (1/(np.sqrt(2*np.pi))*f1*f2) * np.exp(-((O1-u1)**2/2*f1**2)-((O2-u2)**2/2*f2**2))
     result= G
     return result
-#Psi_star = lambda x1, x2: np.conj(Psi(x1, x2))
 
 
 O1, O2 = np.meshgrid(o1, o2, indexing='ij')  # full 2D grid versions of x and p  #Grid for X,P
 P = Psi(O1,O2,f1,f2,u1,u2).astype(complex)
-
+U = -m1*g*l1*np.cos(O1) - m2*g*(l1*np.cos(O1) + l2*np.cos(O2))
 
 def second_derivative_4th_order_parallel(P_array, axis, spacing, n_jobs=-1):
     result = np.zeros_like(P_array, dtype=complex)
@@ -132,7 +129,6 @@ def mixed_second_derivative_4th_order_parallel(P_array, dx1, dx2, n_jobs=-1):
     return result
 
 
-
 # Time derivative function for RK4
 def f(P_array):
     d2P_dO1 = second_derivative_4th_order_parallel(P_array, axis=0, spacing=dO1)
@@ -162,195 +158,53 @@ rk4_end = time.time()
 rk4_time = rk4_end - rk4_start
 
 
-'''
 #Wigner Conversion
-def Wigner(P_grid, x1, x2, p1, p2, y1_vals, y2_vals, h_bar=1):
-    print("\n🎬 Starting parallel Wigner computation...")
+def Wigner(P, o1, o2, n1, n2, y1_vals, y2_vals, h_bar=1):
     t0 = time.time()
+    result = np.zeros((len(o1), len(o2), len(n1), len(n2)), dtype=complex)
 
-    n_x1, n_x2 = len(x1), len(x2)
-    n_p1, n_p2 = len(p1), len(p2)
-
-    result = np.zeros((n_x1, n_x2, n_p1, n_p2))
+    # Create interpolator for Psi (works for complex functions)
+    interp = RectBivariateSpline(o1, o2, P)
 
     def compute_element(i, j, k, l):
+        o1_val = o1[i]
+        o2_val = o2[j]
+        n1_val = n1[k]
+        n2_val = n2[l]
+
         Y1, Y2 = np.meshgrid(y1_vals, y2_vals, indexing='ij')
 
-        # Shifted indices (simplified assumption: Psi constant around grid point)
-        psi_plus  = P_grid[i, j]  # Ideally: interpolated at (x1[i] + Y1, x2[j] + Y2)
-        psi_minus = P_grid[i, j]  # Here we avoid full interpolation for speed
+        # Calculate ψ*(a1+y1, a2+y2) * ψ(a1-y1, a2-y2) * exp(2i(n1y1 + n2y2))
+        psi_plus = interp(o1_val + Y1, o2_val + Y2, grid=False)
+        psi_minus = interp(o1_val - Y1, o2_val - Y2, grid=False)
+        integrand = np.conj(psi_plus) * psi_minus * np.exp(2j*(n1_val*Y1 + n2_val*Y2))
 
-        integrand = np.real(
-            np.conj(psi_plus) * psi_minus *
-            np.exp(2j * (p1[k] * Y1 + p2[l] * Y2) / h_bar)
-        )
-
+        # Double integration
         integral_y2 = simps(integrand, y2_vals, axis=1)
         integral = simps(integral_y2, y1_vals)
-        return (i, j, k, l, integral / ((np.pi * h_bar) ** 2))
 
-    indices = [(i, j, k, l) for i in range(n_x1)
-                                for j in range(n_x2)
-                                for k in range(n_p1)
-                                for l in range(n_p2)]
+        norm = 1 / (np.pi * h_bar)**2
+        return (i, j, k, l, norm * integral)
+
+    # Parallel computation
+    indices = [(i,j,k,l) for i in range(len(o1))
+                        for j in range(len(o2))
+                        for k in range(len(n1))
+                        for l in range(len(n2))]
 
     results = Parallel(n_jobs=-1, verbose=0)(
-        delayed(compute_element)(i, j, k, l) for (i, j, k, l) in indices
+        delayed(compute_element)(*idx) for idx in indices
     )
 
     for i, j, k, l, val in results:
         result[i, j, k, l] = val
 
-    t1 = time.time()
-    wigner_time = t1 - t0
-    print("✅ Parallel Wigner computation completed in {:.2f} seconds.\n".format(wigner_time))
-    return result, wigner_time
-
-W, wigner_time = Wigner(P, o1, o2, p1, p2, y1_vals, y2_vals, h_bar=1)
-'''
+    print(f"\n✅ Wigner computed in {time.time()-t0:.2f}s")
+    return result.real, time.time()-t0  # Return real part as Wigner function should be real
+W, wigner_time = Wigner(P, o1, o2, n1, n2, y1_vals, y2_vals, h_bar=1)
 
 
 print("n_boxes =", n_o1, "; n_y =", n_y, "; n_t =", n_t, "; Time,t =", t_f)
-
-#np.save("W_output.npy", W)
-
-i1 = np.argmin(np.abs(o1 - 0))
-j1 = np.argmin(np.abs(o2 - 0.2))
-print("\nP_derived value at closest (x1=%.2f, x2=%.2f):" % (o1[i1], o2[j1]), P[i1, j1])
-
-i1 = np.argmin(np.abs(o1 - 0))
-j1 = np.argmin(np.abs(o2 - 1))
-print("\nP_derived value at closest (x1=%.2f, x2=%.2f):" % (o1[i1], o2[j1]), P[i1, j1])
-
-i1 = np.argmin(np.abs(o1 - (0)))
-j1 = np.argmin(np.abs(o2 - (-0.2)))
-print("\nP_derived value at closest (x1=%.2f, x2=%.2f):" % (o1[i1], o2[j1]), P[i1, j1])
-
-i1 = np.argmin(np.abs(o1 - (0)))
-j1 = np.argmin(np.abs(o2 - (-1)))
-print("\nP_derived value at closest (x1=%.2f, x2=%.2f):" % (o1[i1], o2[j1]), P[i1, j1])
-
-'''
-i1 = np.argmin(np.abs(o1 - (-0.4)))
-j1 = np.argmin(np.abs(o2 - (-0.4)))
-k1 = np.argmin(np.abs(p1 - (0)))
-l1 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i1],o2[j1],p1[k1],p2[l1]), W[i1,j1,k1,l1])
-
-i2 = np.argmin(np.abs(o1 - (0.4)))
-j2 = np.argmin(np.abs(o2 - (0.4)))
-k2 = np.argmin(np.abs(p1 - (0)))
-l2 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i2],o2[j2],p1[k2],p2[l2]), W[i2,j2,k2,l2])
-
-i3 = np.argmin(np.abs(o1 - (-0.2)))
-j3 = np.argmin(np.abs(o2 - (-0.2)))
-k3 = np.argmin(np.abs(p1 - (0)))
-l3 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i3],o2[j3],p1[k3],p2[l3]), W[i3,j3,k3,l3])
-
-i4 = np.argmin(np.abs(o1 - (0.1)))
-j4 = np.argmin(np.abs(o2 - (0.1)))
-k4 = np.argmin(np.abs(p1 - (0)))
-l4 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i4],o2[j4],p1[k4],p2[l4]), W[i4,j4,k4,l4])
-
-i5 = np.argmin(np.abs(o1 - (-0.1)))
-j5 = np.argmin(np.abs(o2 - (-0.1)))
-k5 = np.argmin(np.abs(p1 - (0)))
-l5 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i5],o2[j5],p1[k5],p2[l5]), W[i5,j5,k5,l5])
-
-i6 = np.argmin(np.abs(o1 - (-1)))
-j6 = np.argmin(np.abs(o2 - (0)))
-k6 = np.argmin(np.abs(p1 - (-0.5)))
-l6 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i6],o2[j6],p1[k6],p2[l6]), W[i6,j6,k6,l6])
-
-i7 = np.argmin(np.abs(o1 - (-0.8)))
-j7 = np.argmin(np.abs(o2 - (0)))
-k7 = np.argmin(np.abs(p1 - (-0.5)))
-l7 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i8 = np.argmin(np.abs(o1 - (-0.5)))
-j8 = np.argmin(np.abs(o2 - (0)))
-k8 = np.argmin(np.abs(p1 - (-0.5)))
-l8 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i8],o2[j8],p1[k8],p2[l8]), W[i8,j8,k8,l8])
-
-i9 = np.argmin(np.abs(o1 - (-0.1)))
-j9 = np.argmin(np.abs(o2 - (0)))
-k9 = np.argmin(np.abs(p1 - (-0.1)))
-l9 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i9],o2[j9],p1[k9],p2[l9]), W[i9,j9,k9,l9])
-
-i10 = np.argmin(np.abs(o1 - (0.1)))
-j10 = np.argmin(np.abs(o2 - (0)))
-k10 = np.argmin(np.abs(p1 - (0.1)))
-l10 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i10],o2[j10],p1[k10],p2[l10]), W[i10,j10,k10,l10])
-
-i11 = np.argmin(np.abs(o1 - (0.3)))
-j11 = np.argmin(np.abs(o2 - (0)))
-k11 = np.argmin(np.abs(p1 - (0.3)))
-l11 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i11],o2[j11],p1[k11],p2[l11]), W[i11,j11,k11,l11])
-
-i12 = np.argmin(np.abs(o1 - (0.5)))
-j12 = np.argmin(np.abs(o2 - (0)))
-k12 = np.argmin(np.abs(p1 - (0.5)))
-l12 = np.argmin(np.abs(p2 - (0)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i12],o2[j12],p1[k12],p2[l12]), W[i12,j12,k12,l12])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (1)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (0.5)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (-1)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (-1)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (1)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (0.5)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (-1)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (-1)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (-0.5)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (-0.5)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (0.5)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (0.5)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (0.3)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (0.3)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-
-i7 = np.argmin(np.abs(o1 - (0)))
-j7 = np.argmin(np.abs(o2 - (0.1)))
-k7 = np.argmin(np.abs(p1 - (0)))
-l7 = np.argmin(np.abs(p2 - (-0.1)))
-print("\nW_derived value at closest (x1=%.2f,x2=%.2f,p1=%.2f,p2=%.2f):" % (o1[i7],o2[j7],p1[k7],p2[l7]), W[i7,j7,k7,l7])
-'''
 
 total_end = time.time()
 total_time = total_end - total_start
