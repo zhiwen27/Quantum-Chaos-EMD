3c1b0a79a722556af767f73cf2d7c0959102018b
Zhiwen Ji <zji@smith.edu>
2025-07-23 22:23:13 +0800

    1st try

--- Double_Pendulum_4D.py
+++ Double_Pendulum_4D.py
@@ -13,6 +13,7 @@ MAX_DIM = 4
 q = Queue(maxsize=1)
 
 def wignerTimeEvol(q):
+    start_time = time.time()
     m1=1000
     m2=1
     g=10
@@ -47,12 +48,12 @@ def wignerTimeEvol(q):
     dO1 = (o1_max - o1_min) / (n_o1 - 1)
     dO2 = (o2_max - o2_min) / (n_o2 - 1)
 
+    t = 1
+    t_f = 0.1
+    n_t = 100
+    dt = t_f / n_t
+    n_t=math.ceil(t_f/dt)
 
-    t_f=0  #final time
-    dt=0.0001
-    n_t=math.ceil(t_f/dt) #Round Up
-
-    # Create coordinate grids
     o1 = np.linspace(o1_min, o1_max, n_o1)
     o2 = np.linspace(o2_min, o2_max, n_o2)
 
@@ -63,24 +64,27 @@ def wignerTimeEvol(q):
 
 
     def Psi_src(O1,O2,f1,f2,u1,u2):
-        G = (1/(np.sqrt(2*np.pi))*f1*f2) * np.exp(-((O1-u1)**2/2*f1**2)-((O2-u2)**2/2*f2**2))
+        G = (1/(np.sqrt(2*np.pi*f1*f2))) * np.exp(-((O1-u1)**2/2*f1**2)-((O2-u2)**2/2*f2**2))
         return G
-
+    
+    O1, O2 = np.meshgrid(o1, o2, indexing='ij')  # full 2D grid versions of x and p  #Grid for X,P
     P_src = Psi_src(O1,O2,f1,f2,u1,u2).astype(complex)
     f1=0.00012
     f2=0.12
 
     def Psi_dest(O1,O2,f1,f2,u1,u2):
-        G = (1/(np.sqrt(2*np.pi))*f1*f2) * np.exp(-((O1-u1)**2/2*f1**2)-((O2-u2)**2/2*f2**2))
+        G = (1/(np.sqrt(2*np.pi*f1*f2))) * np.exp(-((O1-u1)**2/2*f1**2)-((O2-u2)**2/2*f2**2))
         return G
     
-    O1, O2 = np.meshgrid(o1, o2, indexing='ij')  # full 2D grid versions of x and p  #Grid for X,P
     P_dest = Psi_dest(O1,O2,f1,f2,u1,u2).astype(complex)
     U = -m1*g*l1*np.cos(O1) - m2*g*(l1*np.cos(O1) + l2*np.cos(O2))
 
     def second_derivative_4th_order_parallel(P_array, axis, spacing, n_jobs=-1):
         result = np.zeros_like(P_array, dtype=complex)
         shape = P_array.shape
+
+        def p(i):
+            return i % shape[axis]
 
         def compute_slice(i):
             slc1 = [slice(None)] * 2
@@ -89,11 +93,11 @@ def wignerTimeEvol(q):
             slc4 = [slice(None)] * 2
             center = [slice(None)] * 2
 
-            slc1[axis] = i - 2
-            slc2[axis] = i - 1
-            slc3[axis] = i + 1
-            slc4[axis] = i + 2
-            center[axis] = i
+            slc1[axis] = p(i - 2)
+            slc2[axis] = p(i - 1)
+            slc3[axis] = p(i + 1)
+            slc4[axis] = p(i + 2)
+            center[axis] = p(i)
 
             val = (-P_array[tuple(slc4)] + 16*P_array[tuple(slc3)] - 30*P_array[tuple(center)] +
                 16*P_array[tuple(slc2)] - P_array[tuple(slc1)]) / (12 * spacing**2)
@@ -112,13 +116,19 @@ def wignerTimeEvol(q):
         result = np.zeros_like(P_array, dtype=complex)
         shape = P_array.shape
 
-        def compute_point(i, j):
-            val = (
-                +   P_array[i-2,j-2] - 8*P_array[i-2,j-1] + 8*P_array[i-2,j+1] -   P_array[i-2,j+2]
-                - 8*P_array[i-1,j-2] +64*P_array[i-1,j-1] -64*P_array[i-1,j+1] + 8*P_array[i-1,j+2]
-                + 8*P_array[i+1,j-2] -64*P_array[i+1,j-1] +64*P_array[i+1,j+1] - 8*P_array[i+1,j+2]
-                -   P_array[i+2,j-2] + 8*P_array[i+2,j-1] - 8*P_array[i+2,j+1] +   P_array[i+2,j+2]
-            )
+        def p1(i):
+            return i % P_array.shape[0]
+        
+        def p2(i):
+            return i % P_array.shape[1]
+
+        def compute_point(i,j):
+            val = (P_array[p1(i - 2), p2(j - 2)] - 8 * P_array[p1(i - 1), p2(j - 2)] + 8 * P_array[p1(i + 1), p2(j - 2)] -
+                P_array[p1(i + 2), p2(j - 2)] - 8 * P_array[p1(i - 2), p2(j - 1)] + 64 * P_array[p1(i - 1), p2(j - 1)] -
+                64 * P_array[p1(i + 1), p2(j - 1)] + 8 * P_array[p1(i + 2), p2(j - 1)] + 8 * P_array[p1(i - 2), p2(j + 1)] -
+                64 * P_array[p1(i - 1), p2(j + 1)] + 64 * P_array[p1(i + 1), p2(j + 1)] - 8 * P_array[p1(i + 2), p2(j + 1)] -
+                P_array[p1(i - 2), p2(j + 2)] + 8 * P_array[p1(i - 1), p2(j + 2)] - 8 * P_array[p1(i + 1), p2(j + 2)] +
+                P_array[p1(i + 2), p2(j + 2)])
             return (i, j, val / (144 * dx1 * dx2))
 
         # Only compute for valid interior points
@@ -138,18 +148,19 @@ def wignerTimeEvol(q):
 
         H = A*d2P_dO1 + B*d2P_dO2 - C*d2P_dO1_dO2 + U
         return (H*P_array/(1j*h_bar))
+    
+    def timeEvol(P):
+        for step in range(n_t):
 
-    for step in range(n_t):
-
-        k1=f(P)
-        P1=P+k1*dt/2
-        k2=f(P1)
-        P2=P+k2*dt/2
-        k3=f(P2)
-        P3=P+k3*dt
-        k4=f(P3)
-        P=P+(dt/6)*(k1+2*k2+2*k3+k4)
-        P[:2, :] = P[-2:, :] = P[:, :2] = P[:, -2:] = 0
+            k1=f(P)
+            P1=P+k1*dt/2
+            k2=f(P1)
+            P2=P+k2*dt/2
+            k3=f(P2)
+            P3=P+k3*dt
+            k4=f(P3)
+            P=P+(dt/6)*(k1+2*k2+2*k3+k4)
+        return P
 
     def Wigner(P, o1, o2, n1, n2, y1_vals, y2_vals, h_bar=1):
         t0 = time.time()
@@ -188,8 +199,21 @@ def wignerTimeEvol(q):
             result[i, j, k, l] = val
 
         return result.real
-    Psi_src = Wigner(P_src, o1, o2, n1, n2, y1_vals, y2_vals, h_bar=1)
-    Psi_dest = Wigner(P_dest, o1, o2, n1, n2, y1_vals, y2_vals, h_bar=1)
+    source = Wigner(P_src, o1, o2, n1, n2, y1_vals, y2_vals, h_bar=1)
+    dest = Wigner(P_dest, o1, o2, n1, n2, y1_vals, y2_vals, h_bar=1)
+    q.put((0,source.copy(),dest.copy()))
+
+    for i in range (int(t/t_f)):
+        P_src = timeEvol(P_src)
+        P_dest = timeEvol(P_dest)
+        source = Wigner(P_src, o1, o2, n1, n2, y1_vals, y2_vals, h_bar)
+        dest = Wigner(P_dest, o1, o2, n1, n2, y1_vals, y2_vals, h_bar)
+        q.put(((i + 1) * t_f,source.copy(),dest.copy()))
+    q.put(None)
+
+    end_time = time.time()
+    print(end_time-start_time)
+
 
 def emdCal(q):
     import cupy as cp
